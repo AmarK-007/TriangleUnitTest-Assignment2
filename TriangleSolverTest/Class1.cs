a008b3d3e40f6ae4450976e44c3b595789b7a5c1
AK <Amarnath@LAPTBLR172.octasic.com>
2024-03-04 16:20:14 -0500

    Added Equilateral Triangle Test case

---
+++ TriangleSolverTest/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleSolverTest
+{
+    [TestFixture]
+    public class EquilateralTriangleTest {
+        [Test]
+        public void AnalyzeTriangle_Input30and30and30_ReturnsEquilateralTriangle() {
+            //Arrange
+            int firstSide = 30;
+            int secondSide = 30;
+            int thirdSide = 30;
+            string expectedResult = "Equilateral triangle";
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+    
+}
