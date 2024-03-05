c10e9e6e44e75a03f0f357c443fc9a51df86f8c6
Amarnath Kathiresan <k.amarnath7@gmail.com>
2024-03-05 16:40:52 -0500

    Added Zero side and invalid Triangle Test case with logs

--- TriangleSolverTest/Class1.cs
+++ TriangleSolverTest/Class1.cs
@@ -18,8 +18,14 @@ namespace TriangleSolverTest {
             int secondSide = 30;
             int thirdSide = 30;
             string expectedResult = "Equilateral triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Equilateral Test with sides 30, 30, 30");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
@@ -28,38 +34,56 @@ namespace TriangleSolverTest {
         
         ///	Three (3) tests for a valid isosceles triangle
         [Test]
-        public void AnalyzeTriangle_Input30and30and40_ReturnsIsoscelesTriangle_sideA() {
+        public void AnalyzeTriangle_Input30and30and40_ReturnsIsoscelesTriangle_SideA() {
             //Arrange
             int sideA = 40;
             int sideB = 30;
             int sideC = 30;
             string expectedResult = "Isosceles triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Isosceles Test with sides 40, 30, 30");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
         [Test]
-        public void AnalyzeTriangle_Input30and40and30_ReturnsIsoscelesTriangle_sideB() {
+        public void AnalyzeTriangle_Input30and40and30_ReturnsIsoscelesTriangle_SideB() {
             //Arrange
             int sideA = 30;
             int sideB = 40;
             int sideC = 30;
             string expectedResult = "Isosceles triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Isosceles Test with sides 30, 40, 30");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
         [Test]
-        public void AnalyzeTriangle_Input40and30and30_ReturnsIsoscelesTriangle_sideC() {
+        public void AnalyzeTriangle_Input40and30and30_ReturnsIsoscelesTriangle_SideC() {
             //Arrange
             int sideA = 30;
             int sideB = 30;
             int sideC = 40;
             string expectedResult = "Isosceles triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Isosceles Test with sides 30, 30, 40");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
@@ -75,11 +99,18 @@ namespace TriangleSolverTest {
             int sideB = 40;
             int sideC = 50;
             string expectedResult = "Scalene triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Scalene Test with sides 30, 40, 50");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
+
         [Test]
         public void AnalyzeTriangle_Input40and30and50_ReturnsScaleneTriangle() {
             //Arrange
@@ -87,11 +118,18 @@ namespace TriangleSolverTest {
             int sideB = 30;
             int sideC = 50;
             string expectedResult = "Scalene triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Scalene Test with sides 40, 30, 50");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
+
         [Test]
         public void AnalyzeTriangle_Input40and50and30_ReturnsScaleneTriangle() {
             //Arrange
@@ -99,11 +137,18 @@ namespace TriangleSolverTest {
             int sideB = 50;
             int sideC = 30;
             string expectedResult = "Scalene triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Scalene Test with sides 40, 50, 30");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
+
         [Test]
         public void AnalyzeTriangle_Input50and30and40_ReturnsScaleneTriangle() {
             //Arrange
@@ -111,11 +156,18 @@ namespace TriangleSolverTest {
             int sideB = 30;
             int sideC = 40;
             string expectedResult = "Scalene triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Scalene Test with sides 50, 30, 40");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
+
         [Test]
         public void AnalyzeTriangle_Input50and40and30_ReturnsScaleneTriangle() {
             //Arrange
@@ -123,11 +175,137 @@ namespace TriangleSolverTest {
             int sideB = 40;
             int sideC = 30;
             string expectedResult = "Scalene triangle";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Scalene Test with sides 50, 40, 30");
+
             //Act
             string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+    public class ZeroSideTriangleTest {
+        ///	Three (3) tests for verifying a zero length for one or more sides
+
+        [Test]
+        public void AnalyzeTriangle_Input0and30and30_ReturnsInvalidTriangle_SideA() {
+            //Arrange
+            int sideA = 0;
+            int sideB = 30;
+            int sideC = 30;
+            string expectedResult = "Invalid Triangle - a zero has been detected";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Zero Side Test with sides 0, 30, 30");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input30and0and30_ReturnsInvalidTriangle_SideB() {
+            //Arrange
+            int sideA = 30;
+            int sideB = 0;
+            int sideC = 30;
+            string expectedResult = "Invalid Triangle - a zero has been detected";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Zero Side Test with sides 30, 0, 30");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input30and30and0_ReturnsInvalidTriangle_SideC() {
+            //Arrange
+            int sideA = 30;
+            int sideB = 30;
+            int sideC = 0;
+            string expectedResult = "Invalid Triangle - a zero has been detected";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Zero Side Test with sides 30, 30, 0");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
     }
 
+    public class InvalidTriangleTest {
+        ///	Three (3) tests for verifying an invalid response (other than a zero length)
+
+        [Test]
+        public void AnalyzeTriangle_Input10and20and30_ReturnsInvalidTriangle() {
+            //Arrange
+            int sideA = 10;
+            int sideB = 20;
+            int sideC = 30;
+            string expectedResult = "INVALID!!";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Invalid Triangle Test with sides 10, 20, 30");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input20and10and30_ReturnsInvalidTriangle() {
+            //Arrange
+            int sideA = 20;
+            int sideB = 10;
+            int sideC = 30;
+            string expectedResult = "INVALID!!";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Invalid Triangle Test with sides 20, 10, 30");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input20and30and10_ReturnsInvalidTriangle() {
+            //Arrange
+            int sideA = 20;
+            int sideB = 30;
+            int sideC = 10;
+            string expectedResult = "INVALID!!";
+            ///Logging for debug purposes
+            Console.WriteLine("Input: Running Invalid Triangle Test with sides 20, 30, 10");
+
+            //Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            ///Logging the result
+            Console.WriteLine($"Output: Expected: {expectedResult}, Actual: {result}");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
 }
